5dafcca28a8e77c578d8b2146df59befc397ff48
Satvir Sandhu <356Sandhu@gmail.com>
2020-06-27 15:31:39 -0400

    add Vote component

---
+++ src/Vote.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Vote = () => {
+  return (
+    <div className="vote">
+      <h4>Hello</h4>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+};
+
+export default Vote;
