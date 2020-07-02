709e0ba4d60f749ef1705a9a91a64df85476f2b9
Tej Lehal <tejlehal13@gmail.com>
2020-07-01 22:45:45 -0400

    Added Scroll to result, fixed the errors in console log, added sweetalert for error handling

--- src/MainComp.js
+++ src/MainComp.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 
-const MainComp = ({isBookBetter, title, movieImageUrl, bookImageUrl, bookAuthor}) => {
-    
+const MainComp = ({ isBookBetter, title, movieImageUrl, bookImageUrl, bookAuthor, scrollRef }) => {
+
 
   return (
 
-    <div className="mainCompWrapper">
-    {isBookBetter ? <h2>Yup.</h2> : <h2>Nope.</h2> }
-        <h3>{title}</h3>
-        <div className="imagesDiv">
-            <div className="bookDiv">
-                <img src={bookImageUrl} alt={`Book cover art for ${title}`}></img>
-            </div>
-            <div className="movieDiv">
-                <img src={movieImageUrl} alt={`Movie cover art for ${title}`}></img>    
-            </div>
+    <div ref={scrollRef} className="mainCompWrapper">
+      {isBookBetter ? <h2>Yup.</h2> : <h2>Nope.</h2>}
+      <h3>{title}</h3>
+      <div className="imagesDiv">
+        <div className="bookDiv">
+          <img src={bookImageUrl} alt={`Book cover art for ${title}`}></img>
         </div>
-        <h4>Written By</h4>
-        <h5>{bookAuthor}</h5>
+        <div className="movieDiv">
+          <img src={movieImageUrl} alt={`Movie cover art for ${title}`}></img>
+        </div>
+      </div>
+      <h4>Written By</h4>
+      <h5>{bookAuthor}</h5>
     </div>
   );
 };
