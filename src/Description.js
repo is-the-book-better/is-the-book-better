e8fb20f842381a9ac81e7a70664b6a9505fdcd6a
PCU <PCU@PCUs-Mac-mini.local>
2020-07-01 23:37:02 -0400

    added regex to Descriptions to parse text

--- src/Description.js
+++ src/Description.js
@@ -9,8 +9,7 @@ const Description = ({movieDescription, bookDescription, bookTitle, movieTitle})
     <div className="descriptionWrapper">
       <div className="bookWrapper">
           <h2>{bookTitle}</h2>
-          {/* <p>{bookDescription.replace(/<(?:.|\n)*?>/gm, '')}</p> */}
-          <p>{bookDescription.replace(/<[^>]+>/g, '')}</p>
+          <p>{bookDescription.replace(/(<([^>]+)>)/ig,"")}</p>
       </div>
       <div className="movieWrapper">
           <h2>{movieTitle}</h2>
